659431260cfcafcb0960b63b46bf824a595defb0
rinjhasijapati <aastha_sijapati@yahoo.com>
2024-04-03 22:17:06 +0545

    final commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+
+class User extends Authenticable
 {
     use HasFactory;
 
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,49 @@
 <?php
 
 use App\Models\User;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
-
-
-Route::get('/store', function (Request $req) {
-    //check validation
-    $validData = $req->validate([
+Route::get('/signup', function () {
+    return view('register');
+});
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+// for Signup user 
+Route::post('/store', function (Request $req) {
+    $req->validate([
         'email' => 'required',
         'username' => 'required',
         'password' => 'required',
     ]);
+    $parsedData = [
+        'email' => $req->email,
+        'password' => bcrypt($req->password),
+        'username' => $req->username,
+    ];
+    User::create($parsedData);
+    return redirect(route('login'));
+})->name('register-user');
 
-    //if no error
-    User::create($validData);   
 
-    return response('ok');
+Route::post('/signin', function (Request $req) {
+    $req->validate([
+        'email' => ['required', 'email'],
+        'password' => 'required',
+    ]);
+
+    if (Auth::attempt(['email' => $req->email, 'password' => $req->password])) {
+        // password is correct
+        dd('Login vayo');
+    } else {
+        // login failed
+        dd('login failed');
+    }
+
+    return response("la haiii, login garna layo tori le");
 });
